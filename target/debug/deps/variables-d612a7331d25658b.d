8466a95405132e8f682e921a8503d1c934514114
PinaComa <pnarcomak@gmail.com>
2025-07-10 15:03:49 +0200

    First commit - Basics of Rust

---
+++ target/debug/deps/variables-d612a7331d25658b.d
@@ -0,0 +1,5 @@
+/Users/picodc/Desktop/BogdanRust/RUST-Bogdan/basics/target/debug/deps/variables-d612a7331d25658b.d: src/main.rs
+
+/Users/picodc/Desktop/BogdanRust/RUST-Bogdan/basics/target/debug/deps/variables-d612a7331d25658b: src/main.rs
+
+src/main.rs:
---
+++ target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/picodc/Desktop/BogdanRust/RUST-Bogdan/basics/target/debug/variables: /Users/picodc/Desktop/BogdanRust/RUST-Bogdan/basics/src/main.rs
